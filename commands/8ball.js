72fa63e166a0682f58a7bb175e4d199373195dde
Paul Hay <paul.hay@civil.lmco.com>
2016-07-12 17:22:37 +0100

    Not sure what I did tbh, call it tidying up

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,6 +1,5 @@
 'use strict';
 var helper = require('../helper');
-var colour = require('../colour');
 
 (function (actions) {
   var options = ['Aye', 'Naw', 'If you want lad', 'Deffo not u mad man',
