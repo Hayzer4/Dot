5ff9a896a2d7baafdbf6a7cb46150f60ba16e609
Paul Hay <paul.hay@civil.lmco.com>
2016-08-04 11:33:41 +0100

    Finished skip command for skipping nightbot songs

--- commands/skipsong.js
+++ commands/skipsong.js
@@ -1,22 +1,41 @@
 'use strict';
-var helper = require('../helper');
+var skipped = new Array(String);
+var skipCount = 0;
+var date = new Date();
+var voteStarted = null;
+// This code SUCKS but for some reason it wasn't working using the Array as the count
 
 (function (actions) {
-  var skipped = [];
-  var date = new Date;
-  var voteStarted = null;
+
   actions.skip = function (bot, from, to, text, split)
   {
-    skipped += from;
-    if (voteStarted == null || voteStarted - date.now() > 30000)
+    console.log('Entered skip');
+    if (voteStarted == null || (date.getTime() - voteStarted) > 30000)
     {
-      voteStarted = date.now();
-      return 'New vote to skip has been started. Type !skip to skip the song, will expire in 30s.'
+      console.log('entered if');
+      voteStarted = date.getTime();
+      skipped = from;
+      skipCount++;
+      return 'New vote to skip has been started. Type !skip to skip the song, vote expires in 30s.';
+    } else if (skipped.indexOf(from) > -1)
+    {
+      return 'You already voted ' + from + '!';
+    } else {
+      console.log('else');
+      skipped += from;
+      skipCount ++;
     }
 
-    if (skipped.length > 4)
+    if (skipCount > 4)
     {
+      console.log('2nd if');
+      skipped = [];
+      voteStarted = null;
+      skipCount = 0;
       return '!songs skip';
+    } else
+    {
+      return 'Vote recorded ' + from;
     }
   };
 })(module.exports);
